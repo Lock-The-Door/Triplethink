8b8736d3c56fce30b49213bd6d71d4b343cf5cac
Lock-The-Door <Lock-The-Door@outlook.com>
2024-01-16 20:44:47 -0500

    The speech bubble

---
+++ scripts/speech_bubble.gd
@@ -0,0 +1,36 @@
+extends Control
+
+const BUBBLE_PADDING = 10
+
+@export var text: String = "Whoops! I don't know what to say!"
+@export var width: int = 200
+@export var characters_per_second: int = 20
+@export var target: Node = null
+
+@onready var bubble: NinePatchRect = $Bubble
+@onready var arrow: TextureRect = $Bubble/Arrow
+@onready var label: Label = $Bubble/Text
+
+var time = 0.0
+
+func _ready():
+	bubble.size.x = width
+
+	if target == null:
+		arrow.visible = false
+	else:
+		arrow.pivot_offset = Vector2(arrow.size.x / 2, 0)
+	
+
+func _process(delta):
+	time += delta
+
+	label.visible_characters = int(time * characters_per_second)
+
+	if arrow.visible == false:
+		return
+
+	var direction = (target.global_position - bubble.global_position).normalized()
+	var angle = atan2(direction.y, direction.x)
+	# arrow at 0 is pointing down
+	arrow.rotation_degrees = angle * 180 / PI - 90
